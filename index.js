d0d6490be52540d9354f2d445976bb07d0992426
yaswanth <ravulapallibunnyreddy@gmail.com>
2025-04-21 14:11:29 +0530

    final java script code

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-document.getElementById("count").innerText = 5
+let value = document.getElementById("count");
+
+function increment() {
+    value.innerText = parseInt(value.innerText) + 1;
+}
+
+let prev = document.getElementById("Previous_entries");
+
+
+function save() {
+    let countt = value.innerText + " - "; 
+    prev.innerText  += countt;
+    value.innerText = 0;
+}
